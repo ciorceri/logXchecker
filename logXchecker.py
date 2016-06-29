19a3e1613742adaad98eef60058b0f33082b5487
ciorceri <cpsorin@gmail.com>
2016-06-29 23:23:25 +0300

    debug message cleanup

--- logXchecker.py
+++ logXchecker.py
@@ -2,12 +2,30 @@ import argparse
 
 
 class Parser():
+
+    def check_format_value(self, arg):
+        validFormats = ('EDI', 'ADIF', 'CABRILLO')
+        arg = arg.upper()
+        if arg in validFormats:
+            return arg
+        raise argparse.ArgumentTypeError('Format "%s" is an invalid value' % arg)
+
+    def check_output_value(self, arg):
+        validOutput = ('TXT', 'TEXT', 'HTML', 'JSON', 'YML')
+        arg = arg.upper()
+        if arg in validOutput:
+            return arg
+        raise argparse.ArgumentTypeError('Output "%s" is an invalid value' % arg)
+
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='log cross checker')
-        self.parser.add_argument('-f', '--format', type=str, required=True, help="Log format")
+        self.parser.add_argument('-f', '--format', type=self.check_format_value, required=True, help="Log format: edi, adif, cabrillo")
+        self.parser.add_argument('-r', '--rules', type=str, required=False, help='INI file with contest rules')
+        self.parser.add_argument('-o', '--output', type=self.check_output_value, required=False, help='Output format: text, html, json, yml')
         group = self.parser.add_mutually_exclusive_group()
         group.add_argument('-slc', '--singlelogcheck', action='store_true', default=False)
         group.add_argument('-mlc', '--multilogcheck', action='store_true', default=False)
+
 
     def parse(self, commandLine):
         return self.parser.parse_args(commandLine)
--- test_parser.py
+++ test_parser.py
@@ -3,32 +3,42 @@ from logXchecker import Parser
 
 
 class TestParser(TestCase):
-    testcase_with_errors = (('', 2), ('hello', 2), ('-h', 2), ('--help', 2),
-                            (['-h'], 0), (['--help'], 0),
-                            (['-f'], 2), (['-f edi'], 0), (['-f', 'edi'], 0),
-                            (['-f=edi', '-slc'], 0),
-                            (['-f=edi', '--singlelogcheck'], 0),
-                            (['-f=edi', '-mlc'], 0),
-                            (['-f=edi', '--multilogcheck'], 0),
-                            (['-f=edi', '-slc', '-mlc'], 2),
-                            )
-    testcase_with_success = ((['-f=edi'], 'edi', False, False), (['-fedi'], 'edi', False, False),
-                             (['-f=edi', '-slc'], 'edi', True, False),
-                             (['-f=edi', '--singlelogcheck'], 'edi', True, False),
-                             (['-f=edi', '-mlc'], 'edi', False, True),
-                             (['-f=edi', '--multilogcheck'], 'edi', False, True),
+    testcase_with_error_checks = (('', 2), ('hello', 2), ('-h', 2), ('--help', 2),
+                                  (['-h'], 0), (['--help'], 0),
+                                  (['-f=edi'], 0), (['-fedi'], 0), (['-f', 'edi'], 0),
+                                  (['-f=adif'], 0), (['-fadif'], 0), (['-f', 'adif'], 0),
+                                  (['-f=cabrillo'], 0), (['-fcabrillo'], 0), (['-f', 'cabrillo'], 0),
+                                  (['-f'], 2), (['-f edi'], 2), (['-f hello'], 2),
+                                  (['-f=edi', '-o=text'], 0), (['-f=edi', '-otext'], 0),
+                                  (['-f=edi', '-o'], 2),
+                                  (['-f=edi', '-o=hello'], 2), (['-f=edi', '-ohello'], 2),
+                                  (['-f=edi', '-o', 'hello'], 2),
+                                  (['-f=edi', '-slc'], 0),
+                                  (['-f=edi', '--singlelogcheck'], 0),
+                                  (['-f=edi', '-mlc'], 0),
+                                  (['-f=edi', '--multilogcheck'], 0),
+                                  (['-f=edi', '-slc', '-mlc'], 2),
+                                  )
+    testcase_with_success = ((['-f=edi'], 'EDI', False, False), (['-fedi'], 'EDI', False, False),
+                             (['-f=edi', '-slc'], 'EDI', True, False),
+                             (['-f=edi', '--singlelogcheck'], 'EDI', True, False),
+                             (['-f=edi', '-mlc'], 'EDI', False, True),
+                             (['-f=edi', '--multilogcheck'], 'EDI', False, True),
                              )
     def setUp(self):
         self.p = Parser()
 
     def test_parse(self):
-        for (arg,exitCode) in self.testcase_with_errors:
+        for (arg,exitCode) in self.testcase_with_error_checks:
             try:
                 print('testing parser agument:', arg)
                 parsed = self.p.parse(arg)
             except SystemExit as e:
                 print('error code:', e.code, '| expected error code:', exitCode)
                 self.assertEqual(e.code, exitCode)
+            else: # only tests with expected exitCode = 0 should get here
+                if exitCode != 0:
+                    raise ValueError('Should exit with exit code 0, but it didn\'t')
 
         for (arg,format,singlelogcheck,multilogcheck) in self.testcase_with_success:
             print('testing parser argument:', arg)
