84b70d101b3514d4aae939daa7307f766315f242
ciorceri <cpsorin@gmail.com>
2016-07-06 19:41:28 +0300

    PEP8

--- logXchecker.py
+++ logXchecker.py
@@ -4,28 +4,29 @@ import argparse
 class Parser():
 
     def check_format_value(self, arg):
-        validFormats = ('EDI', 'ADIF', 'CABRILLO')
+        valid_formats = ('EDI', 'ADIF', 'CABRILLO')
         arg = arg.upper()
-        if arg in validFormats:
+        if arg in valid_formats:
             return arg
         raise argparse.ArgumentTypeError('Format "%s" is an invalid value' % arg)
 
     def check_output_value(self, arg):
-        validOutput = ('TXT', 'TEXT', 'HTML', 'JSON', 'YML')
+        valid_output = ('TXT', 'TEXT', 'HTML', 'JSON', 'YML')
         arg = arg.upper()
-        if arg in validOutput:
+        if arg in valid_output:
             return arg
         raise argparse.ArgumentTypeError('Output "%s" is an invalid value' % arg)
 
     def __init__(self):
         self.parser = argparse.ArgumentParser(description='log cross checker')
-        self.parser.add_argument('-f', '--format', type=self.check_format_value, required=True, help="Log format: edi, adif, cabrillo")
+        self.parser.add_argument('-f', '--format', type=self.check_format_value, required=True,
+                                 help="Log format: edi, adif, cabrillo")
         self.parser.add_argument('-r', '--rules', type=str, required=False, help='INI file with contest rules')
-        self.parser.add_argument('-o', '--output', type=self.check_output_value, required=False, help='Output format: text, html, json, yml')
+        self.parser.add_argument('-o', '--output', type=self.check_output_value, required=False,
+                                 help='Output format: text, html, json, yml')
         group = self.parser.add_mutually_exclusive_group()
         group.add_argument('-slc', '--singlelogcheck', action='store_true', default=False)
         group.add_argument('-mlc', '--multilogcheck', action='store_true', default=False)
 
-
-    def parse(self, commandLine):
-        return self.parser.parse_args(commandLine)
+    def parse(self, command_line):
+        return self.parser.parse_args(command_line)
--- test_parser.py
+++ test_parser.py
@@ -25,22 +25,23 @@ class TestParser(TestCase):
                              (['-f=edi', '-mlc'], 'EDI', False, True),
                              (['-f=edi', '--multilogcheck'], 'EDI', False, True),
                              )
+
     def setUp(self):
         self.p = Parser()
 
     def test_parse(self):
-        for (arg,exitCode) in self.testcase_with_error_checks:
+        for (arg, exitCode) in self.testcase_with_error_checks:
             try:
                 print('testing parser agument:', arg)
                 parsed = self.p.parse(arg)
             except SystemExit as e:
                 print('error code:', e.code, '| expected error code:', exitCode)
                 self.assertEqual(e.code, exitCode)
-            else: # only tests with expected exitCode = 0 should get here
+            else:  # only tests with expected exitCode = 0 should get here
                 if exitCode != 0:
                     raise ValueError('Should exit with exit code 0, but it didn\'t')
 
-        for (arg,format,singlelogcheck,multilogcheck) in self.testcase_with_success:
+        for (arg, format, singlelogcheck, multilogcheck) in self.testcase_with_success:
             print('testing parser argument:', arg)
             result = self.p.parse(arg)
             self.assertEqual(result.format, format)
