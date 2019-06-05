865b9e78bf9df6c20087d8ba96ae10b08c899e36
ciorceri <cpsorin@gmail.com>
2019-06-05 20:45:15 +0300

    Updated version -> 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ base = 'Console'
 executables = [Executable("logXchecker.py", base=base)]
 setup(
     name = "logXchecker",
-    version = "1.0",
+    version = "1.1",
     description = 'ham radio log cross checker',
     executables = executables
 )
